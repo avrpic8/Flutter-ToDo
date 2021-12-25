bafee1368ad1c48d961ef63783515dea932002f6
avrpic8 <avrpic8@gmail.com>
2021-12-25 17:46:50 +0330

    project complete

---
+++ lib/app/module/report/report_controller.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+import 'package:to_do/app/data/models/task.dart';
+
+class ReportController extends GetxController{
+
+  final List<Task> tasks;
+
+  ReportController({required this.tasks});
+
+  int getTotalTask() {
+    int res = 0;
+    for (int i = 0; i < tasks.length; i++) {
+      if (tasks[i].todos != null) {
+        res += tasks[i].todos!.length;
+      }
+    }
+    return res;
+  }
+
+  int getTotalDoneTask() {
+    int res = 0;
+    for (int i = 0; i < tasks.length; i++) {
+      if (tasks[i].todos != null) {
+        for (int j = 0; j < tasks[i].todos!.length; j++) {
+          if (tasks[i].todos![j]['done'] == true) {
+            res += 1;
+          }
+        }
+      }
+    }
+    return res;
+  }
+}
